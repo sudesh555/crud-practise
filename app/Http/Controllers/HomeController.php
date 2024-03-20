802d866a667095256c3c33104df5a537f46f1b18
sude5h <sudeshbhandari555@gmail.com>
2024-03-20 08:16:27 +0545

    Added spatie permission and roles

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -23,6 +23,8 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        // Fetch all students from the database
+        $users = User::all();
+        return view('home', ['users' => $users]);
     }
 }
